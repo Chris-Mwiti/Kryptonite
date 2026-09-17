a19b5b79cb26f229dd6caf82c3042de57d7933da
Chris-Mwiti <chrismwiti21@gmail.com>
2026-09-17 23:11:17 +0300

    lesson: Learnt about the fixedBufferAllocator: can accomodate both the stack and heap allocation strategy, am trying to get hold of how can I change values of the input after allocation

--- src/main.zig
+++ src/main.zig
@@ -27,6 +27,7 @@ pub fn main(init: std.process.Init) !void {
     const stdout_writer = &stdout_file_writer.interface;
 
     try Kryptonite.printAnotherMessage(stdout_writer);
+    try Kryptonite.fixedBufferAllocator();
 
     try stdout_writer.flush(); // Don't forget to flush!
 }
--- src/root.zig
+++ src/root.zig
@@ -22,3 +22,17 @@ pub fn printAnotherMessage(writer: *std.Io.Writer) !void {
     try writer.flush();
 }
 
+pub fn fixedBufferAllocator() !void {
+    var buffer: [10]u8 = undefined;
+
+    for(0..buffer.len) |i| {
+        buffer[i] = 0;
+    }
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    const allocator = fba.allocator();
+    const input = try allocator.alloc(u8, 5);
+   
+    defer allocator.free(input);
+
+}
+
