b79b03617b15e03b8fb846e681bbf0cebbce5667
Chris-Mwiti <chrismwiti21@gmail.com>
2026-09-17 21:28:08 +0300

    changes: Learning more about allocators and trying to grasp something out of it

--- src/root.zig
+++ src/root.zig
@@ -4,4 +4,14 @@ const Io = std.Io;
 const net = std.Io.net;
 const posix = std.posix;
 
+pub fn printAnotherMessage(writer: *std.Io.Writer) !void {
+    var gpa = std.heap.DebugAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    const name: [] const u8 = "Chris";
+    const output = try std.fmt.allocPrint(allocator, "Hello {s}!!!", .{name});
+
+    try writer.print("{s}\n", .{output});
+    try writer.flush();
+}
 
